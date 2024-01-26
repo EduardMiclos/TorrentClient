323f4348ef3e55a89eaef1554518d65cf47cf140
EduardMiclos <micloseduard@yahoo.com>
2024-01-26 05:33:48 +0200

    Added read/write to file methods + cache files
    Added read/write byteArray to/from file methods.
    Also added two cache files:
    
    - a cache file for connection information.
    - a cache file for downloading information.
    
    The methods are going to be used to stream information
    to and from the files.

--- src/TorrentTrackerClient.java
+++ src/TorrentTrackerClient.java
@@ -3,10 +3,13 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.util.HexFormat;
-import java.util.Random;
 
 /** This class is responsible for handling the connection with the tracker and extract
  *  useful information to be used for downloading the file pieces. */
@@ -15,19 +18,22 @@ public class TorrentTrackerClient {
 	private InetAddress trackerAddress;
 	private int trackerPort;
 	
-	private byte[] initialConnectionId = new byte[] { (byte)0x00, (byte)0x00, (byte)0x04, (byte)0x17, 
-													  (byte)0x27, (byte)0x10, (byte)0x19, (byte)0x80 };
+	private byte[] initialConnectionId   = new byte[] { (byte)0x00, (byte)0x00, (byte)0x04, (byte)0x17, 
+													    (byte)0x27, (byte)0x10, (byte)0x19, (byte)0x80 };
 	
 	/** Random value. */
 	private byte[] initialTransactionId  = new byte[] { (byte)0x13, (byte)0x07, (byte)0x20, (byte)0x01 };
 	
-	private byte[] connectAction       = new byte[] { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00 };
+	private byte[] connectAction         = new byte[] { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00 };
 	
-	private byte[] announceAction      = new byte[] { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x01 };
+	private byte[] announceAction        = new byte[] { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x01 };
 	
 	
 	private boolean hasConnected = false;
 	private byte[] currentConnectionId = null;
+	
+	private static final String CONNECTION_INFO_CACHEFILE_PATH = "cache/connectionInfo";
+	private static final String DOWNLOAD_INFO_CACHEFILE_PATH = "cache/downloadInfo";
 	
 	public TorrentTrackerClient(String trackerAddress, int trackerPort) throws SocketException {
 		try {
@@ -39,6 +45,29 @@ public class TorrentTrackerClient {
 		
 		this.trackerPort = trackerPort;
 		this.socket = new DatagramSocket();
+	}
+	
+	private byte[] readByteArrayFromFile(String filePath) throws IOException {
+		File fileObject = new File(filePath);
+		if (fileObject.createNewFile()) {
+			System.err.print(String.format("Fatal error: The cache file is empty: %s", filePath));
+			System.exit(1);
+		}
+		
+		FileInputStream inputStream = new FileInputStream(filePath);
+		byte[] byteArr = inputStream.readAllBytes();
+		inputStream.close();
+		
+		return byteArr;
+	}
+	
+	private void writeByteArrayToFile(byte[] byteArr, String filePath) throws IOException {
+		File fileObject = new File(filePath);
+		fileObject.createNewFile();
+		
+		FileOutputStream outputStream = new FileOutputStream(filePath);
+		outputStream.write(byteArr, 0, byteArr.length);
+		outputStream.close();
 	}
 	
 	
@@ -59,6 +88,7 @@ public class TorrentTrackerClient {
 		packet = new DatagramPacket(new byte[16], 0, 16);
 		socket.receive(packet);
 		
+		writeByteArrayToFile(packet.getData(), CONNECTION_INFO_CACHEFILE_PATH);
 		return packet.getData();
 	}
 	
